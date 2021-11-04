1eafc328548710b12e8cd3664bc371ea5b33be2c
Fabian Peters <fabian-peters@users.noreply.github.com>
2021-11-04 20:17:37 +0100

    Change return type to array

--- src/types/history.ts
+++ src/types/history.ts
@@ -5,7 +5,7 @@ export interface History {
   time: number;
 }
 
-export default new Proxy(JSON.parse(fs.readFileSync('./history.json').toString()) as [History], {
+export default new Proxy(JSON.parse(fs.readFileSync('./history.json').toString()) as History[], {
   set: (obj, prop, value) => {
     (obj as any)[prop] = value;
     fs.writeFileSync('./history.json', JSON.stringify(obj, null, '  '));
