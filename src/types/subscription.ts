3d260568433c8c9a214f75ddfc94ce240cea7a48
Fabian Peters <fabian-peters@users.noreply.github.com>
2021-11-05 09:55:47 +0100

    Fix problem trying to read empty history/subs file

--- src/types/subscription.ts
+++ src/types/subscription.ts
@@ -6,7 +6,18 @@ export interface Subscription {
   tier: string;
 }
 
-export default new Proxy(JSON.parse(fs.readFileSync('./subs.json').toString()) as Subscription[], {
+// TODO move to utils
+export function readFile(filename: string) {
+  let fileContent;
+  try {
+    fileContent = fs.readFileSync(filename).toString();
+  } catch (e) {
+    fileContent = "[]"; // TODO use object for config (parameter? 2 methods?)
+  }
+  return JSON.parse(fileContent);
+}
+
+export default new Proxy(readFile('./subs.json') as Subscription[], {
   set: (obj, prop, value) => {
     (obj as any)[prop] = value;
     fs.writeFileSync('./subs.json', JSON.stringify(obj, null, '  '));
