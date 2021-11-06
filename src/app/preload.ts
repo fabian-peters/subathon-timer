e04fdae8f8b1b52644e476be8cf47651cb912ee5
Fabian Peters <fabian-peters@users.noreply.github.com>
2021-11-06 23:35:35 +0100

    Split settings into separate window

--- src/app/preload.ts
+++ src/app/preload.ts
@@ -1,6 +1,6 @@
-import {contextBridge, ipcRenderer} from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('bridge', {
-  send: (channel: string, data: string) => ['config', 'pause', 'stop', 'history-reset', 'history-export', 'subs-reset', 'subs-export', 'settings'].includes(channel) && ipcRenderer.send(channel, data),
-  on: (channel: string, listener: any) => ['config', 'error', 'update-start-times'].includes(channel) && ipcRenderer.on(channel, (_event, val) => listener(val ?? null))
+  send: (channel: string, data: string) => ['pause', 'stop', 'open-settings', 'close-settings', 'save-config', 'history-reset', 'history-export', 'subs-reset', 'subs-export'].includes(channel) && ipcRenderer.send(channel, data),
+  on: (channel: string, listener: any) => ['load-config', 'update-start-times'].includes(channel) && ipcRenderer.on(channel, (_event, val) => listener(val ?? null))
 });
