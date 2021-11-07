024a2671e04f63bb2b5a03942fa26de290824436
Fabian Peters <fabian-peters@users.noreply.github.com>
2021-11-07 23:39:46 +0100

    Show current time in app
    resolves #26

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -12,6 +12,11 @@
   }
 });
 
+(window as any).bridge.on('update-timer', (timeString: string) => {
+  (window as any).timerFront.innerText = timeString;
+  (window as any).timerBackground.innerText = timeString.replace(/\d/g, '8');
+});
+
 ((window as any).pauseButton as HTMLButtonElement).addEventListener('click', () => (window as any).bridge.send('pause'));
 ((window as any).stopButton as HTMLButtonElement).addEventListener('click', () => (window as any).bridge.send('stop')); // TODO add confirmation?
 ((window as any).settingsButton as HTMLButtonElement).addEventListener('click', () => (window as any).bridge.send('open-settings'));
